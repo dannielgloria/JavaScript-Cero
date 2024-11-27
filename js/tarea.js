953b66757ff57e267a139b8c0920e47201a65f55
dannielgloria <danniel.gloria@gmail.com>
2024-11-26 21:40:49 -0600

    Tarea

---
+++ js/tarea.js
@@ -0,0 +1,13 @@
+/*
+Ejercicio de tarea: Generar una tabla de multiplicar (usando funciones)
+Crea un programa que solicite al usuario un número y genere la tabla de multiplicar para ese número. 
+Divide el programa en funciones y usa ciclos para generar los resultados.
+
+Requisitos del ejercicio:
+Usa una función para solicitar el número al usuario.
+Crea otra función para generar y mostrar la tabla de multiplicar.
+El programa debe generar la tabla desde el 1 hasta el 10.
+Asegúrate de usar variables locales para manejar los cálculos.
+
+
+*/
